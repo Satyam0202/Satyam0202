78a7cf708e9c33c69e2612ca90c18d138c3ee2ff
Satyam Singh <m4740600@gmail.com>
2025-10-07 14:23:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,65 @@
-function mode()
+function mode() 
 {
-    const body=document.body;
-    const btn=document.getElementById("mode");
+    const body = document.body;
+    const btn = document.getElementById("mode");
+    body.classList.toggle("light");
+    if(body.classList.contains("light")) 
+    {
+      localStorage.setItem("theme", "light");
+      btn.src = "moon.png";
+    } 
+    else 
+    {
+      localStorage.setItem("theme", "dark");
+      btn.src = "dark.png";
+    }
+}
+function openpop() 
+{
+    document.getElementById("pop-form").style.display = "flex";
+}
+window.onload = function () 
+{
+    const btn = document.getElementById("mode");
+    if (localStorage.getItem("theme") === "light") 
+    {
+      document.body.classList.add("light");
+      btn.src = "moon.png";
+    } 
+    else 
+    {
+      btn.src = "dark.png";
+    }
+};
+// Popup
+setTimeout(() => 
+{
+    document.getElementById("pop-form").style.display = "flex";
+}, 10000);
 
-    document.body.classList.toggle("light");
-    if(document.body.classList.contains("light"))
-    {
-        localStorage.setItem("theme","light");
-        btn.src="moon.png"
-    }
-    else
-    {
-        localStorage.setItem("theme","dark");
-        btn.src="dark.png"
-    }
-}
-window.onload=function()
+function closepop() 
 {
-    const body=document.body;
-    const btn=this.document.getElementById("mode")
-    if(localStorage.getItem("theme")=="light")
+    document.getElementById("pop-form").style.display = "none";
+}
+
+// Sidebar Menu
+function toggleMenu() 
+{
+    const sideMenu = document.getElementById("sideMenu");
+    if(sideMenu.style.width === "250px") 
     {
-        document.body.classList.add("light");
-        btn.src = "moon.png";
-    }
-    else
+      sideMenu.style.width = "0";
+    } 
+    else 
     {
-        btn.src="dark.png";
+      sideMenu.style.width = "250px";
     }
 }
+// TEAM SEMICIRCLE BUTTON
+function toggleTeam() 
+{
+    const container = document.getElementById("teamContainer");
+    container.classList.toggle("active");
+}
+
+
